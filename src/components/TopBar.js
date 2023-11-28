b589175a651c990b9ec96428f1edae5185ebd1a5
MinJaeSon <mjsonsj@gmail.com>
2023-11-28 14:05:16 +0900

    [style] 상단바 속성 추가

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,32 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TopBar = (props) => {
-  return (
-    <Container>
-      {props.children}
-    </Container>
-  );
+  return <Container>{props.children}</Container>;
 };
 
-const Container=styled.div`
+const Container = styled.div`
+  border: 0.5px solid #e1e1e1;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(225, 225, 225, 0.25);
 
-border: 0.5px solid #E1E1E1;
-background: #FFF;
-box-shadow: 0px 4px 4px 0px rgba(225, 225, 225, 0.25);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
+  font-size: 1.4rem;
+  font-weight: 500;
 
-font-size: 1.4rem;
-font-weight: 500;
+  width: 100%;
+  height: 3.75rem;
+  padding: 1rem 0;
+  box-sizing: border-box;
 
-width: 100%;
-height: 3.75rem;
-padding: 1rem 0;
-box-sizing: border-box;
+  form {
+    color: #d9d9d9;
+    font-size: 18px;
+    color: #d9d9d9;
 
+    font-family: Pretendard Variable;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    position: absolute;
+    right: 15px;
+    line-height: 100%;
+  }
 `;
 
 export default TopBar;
